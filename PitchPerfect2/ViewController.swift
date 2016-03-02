5ee836a94dcc9bc8dd66e8077cdea1425b489d5a
Ignacio Giagante <ignacio.giagante@mercadolibre.com>
2016-03-02 12:45:48 -0300

    Initial Commit

---
+++ PitchPerfect2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect2
+//
+//  Created by Ignacio Giagante on 2/3/16.
+//  Copyright © 2016 igiagante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
